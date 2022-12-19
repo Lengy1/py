a211248d06a29256d61d613023d5e40d57740ab8
Lengy1 <812808399@qq.com>
2022-12-19 23:53:54 +0800

    Changes

--- crc明文碰撞.py
+++ crc明文碰撞.py
@@ -3,13 +3,14 @@ import string
 
 def crack_crc():
     print('-------------Start Crack CRC-------------')
-    crc_list = [0x14433530, 0xaf251007, 0xd554e7b6, 0xebb3156, 0xbb474d49, 0x2cb8a39b, 0x75fe76f0]
+    #crc_list = [0x14433530, 0xaf251007, 0xd554e7b6, 0xebb3156, 0xbb474d49, 0x2cb8a39b, 0x75fe76f0]
+    crc_list=[0xe9c8c87d,0x105612c3]
     comment = ''
-    chars = string.printable
+    dic = string.printable
     for crc_value in crc_list:
-        for char1 in chars:
-            for char2 in chars:
-                for char3 in chars:
+        for char1 in dic:
+            for char2 in dic:
+                for char3 in dic:
                     res_char = char1 + char2 + char3
                     char_crc = binascii.crc32(res_char.encode())
                     calc_crc = char_crc & 0xffffffff
